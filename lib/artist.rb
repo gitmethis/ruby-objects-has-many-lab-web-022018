e29d3fa1691b500a80f6d0e05aeb0ac4c4bd6da1
Chase <chaseit12@gmail.com>
2018-03-02 04:13:23 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,6 +16,7 @@ class Artist
   def add_song_by_name(song_name)
      new_song = Song.new(song_name)
      self.songs << new_song
+     new_song.artist = self
      @@song_count << Song.new(new_song)
   end
   
